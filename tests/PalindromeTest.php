4f39f61c835c40c79ca1edaa17f4b3cc899e6f7c
Andrew Devlin and Benjamin T. Seaver <benjamin@>
2017-02-15 14:09:17 -0800

    add test case for alternate method

--- tests/PalindromeTest.php
+++ tests/PalindromeTest.php
@@ -17,10 +17,41 @@
             foreach ($tests as $test) {
                 // Arrange
                 $input = $test[0];
-                $expected_result = $test[1];
+                $expected_result = $test;
 
                 // Act
-                $actual_result = $new_palindrome->palindromeTester($input);
+                $actual_result = array(
+                    $test[0],
+                    $new_palindrome->palindromeTester($input)
+                );
+
+                // Assert
+                $this->assertEquals($expected_result, $actual_result);
+            }
+        }
+
+
+        function test_palindrome_hard()
+        {
+            // Arrange
+            $tests = array(
+                array('bob', true),
+                array('mike', false),
+                array('Bob', true)
+            );
+
+            $new_palindrome = new Palindrome;
+
+            foreach ($tests as $test) {
+                // Arrange
+                $input = $test[0];
+                $expected_result = $test;
+
+                // Act
+                $actual_result = array(
+                    $test[0],
+                    $new_palindrome->palindromeTesterHard($input)
+                );
 
                 // Assert
                 $this->assertEquals($expected_result, $actual_result);
