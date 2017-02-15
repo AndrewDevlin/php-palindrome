806a19d9d713c921248437b9894b60f1095b6703
Andrew Devlin and Benjamin T. Seaver <benjamin@>
2017-02-15 14:08:09 -0800

    complete view with input

--- app/app.php
+++ app/app.php
@@ -13,14 +13,27 @@
     );
 
     $app->get('/', function() use ($app) {
-        $string = "";
-        $result = "";
-        
+        $input_string = "";
+        $string_result = "";
+
+        $input_key = 'string_to_test';
+        if (array_key_exists($input_key, $_GET)) {
+            $input_string = $_GET[$input_key];
+            $new_palindrome = new Palindrome;
+            $boolean_result = $new_palindrome->palindromeTester($input_string) ;
+
+            if ($boolean_result) {
+                $string_result = 'true';
+            } else {
+                $string_result = 'false';
+            }
+        }
+
         return $app['twig']->render(
             'palindrome.html.twig',
             array(
-                "string" => $string,
-                "result" => $result
+                "string" => $input_string,
+                "result" => $string_result
             )
         );
     });
