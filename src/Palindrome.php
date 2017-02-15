8e386037e876c44dfcda7bcf2086ef17114bb7d9
Andrew Devlin and Benjamin T. Seaver <andrew@>
2017-02-15 14:18:25 -0800

    add alternate tester method

--- src/Palindrome.php
+++ src/Palindrome.php
@@ -5,5 +5,16 @@
         {
             return strtolower($string) == strtolower(strrev($string));
         }
+
+        function palindromeTesterHard($string)
+        {
+            $string = strtolower($string);
+            $letters = str_split($string);
+            $reversed_letters = [];
+            foreach ($letters as $letter) {
+                array_unshift($reversed_letters, $letter);
+            }
+            return $string == implode($reversed_letters);
+        }
     }
 ?>
